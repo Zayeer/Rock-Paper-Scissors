40a8c08c1512b84e492bc8e33145b64ec49284f0
Shaik Zayeer Hussain <zayeer.zh@gmail.com>
2020-03-06 15:34:31 +0530

    updated script.js and added box-shadow to choices

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-var container = document.querySelector(".container");
-var rock = document.querySelector('#rock');
-var paper = document.querySelector('#paper');
-var scissors = document.querySelector('#scissors');
-var result = document.querySelector('#result');
-var options = [rock, paper, scissors];
-var compOption;
-var round = document.querySelector('#round');
-var userPoints = document.querySelector("#user-points");
-var computerPoints = document.querySelector("#computer-points");
-var restart = document.querySelector("#restart")
-var clickSound = document.querySelector("#click-sound");
+const container = document.querySelector(".container");
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const choices = document.querySelectorAll('.choice');
+const result = document.querySelector('#result');
+let options = [rock, paper, scissors];
+let compOption;
+let round = document.querySelector('#round');
+let userPoints = document.querySelector("#user-points");
+let computerPoints = document.querySelector("#computer-points");
+let restart = document.querySelector("#restart")
+
 /*computer selects random option*/
-var computerPlay = function () {
+const computerPlay = function () {
 	compOption = options[Math.floor(Math.random() * options.length)];
 	return compOption;
 };
 
 
+
+
 /*below code executes when we click on "rock" button*/
 rock.addEventListener("click", function () {
-	clickSound.play();
+	rock.style.setProperty("box-shadow", "2px 5px grey");
 	computerPlay();
 	if (compOption === rock) {
 		var resultTimerId = setTimeout(function () {
@@ -49,7 +52,7 @@ rock.addEventListener("click", function () {
 /*below code executes when we click on "paper" button*/
 
 paper.addEventListener("click", function () {
-	clickSound.play();
+	paper.style.setProperty("box-shadow", "2px 5px grey");
 	computerPlay();
 	if (compOption === rock) {
 		round.innerHTML = parseInt(round.innerHTML) + 1;
@@ -79,7 +82,7 @@ paper.addEventListener("click", function () {
 
 /*below code executes when we click on "scissors" button*/
 scissors.addEventListener("click", function () {
-	clickSound.play();
+	scissors.style.setProperty("box-shadow", "2px 5px grey");
 	computerPlay();
 	if (compOption === rock) {
 		round.innerHTML = parseInt(round.innerHTML) + 1;
@@ -106,46 +109,50 @@ scissors.addEventListener("click", function () {
 });
 
 /*function to end the game after total 5 rounds completed along with displaying "try again/play again" button*/
-function endOfTheGame() {
+const endOfTheGame = () => {
 	if (round.innerHTML >= "5") {
 		optionButtons.forEach(x => {
 			x.style.cssText = "outline: none";
-		});
-		document.getElementById('rock').disabled = true;
-		document.getElementById('paper').disabled = true;
-		document.getElementById('scissors').disabled = true;
+		});	
+
 		if (computerPoints.innerHTML > userPoints.innerHTML) {
 			var roundsTimerId = setTimeout(function () {
 				result.innerHTML = "GAME OVER!"
 			}, 3000);
 
 			var resultTimerId = setTimeout(function () {
-				result.innerHTML = "You Lose!";
-			}, 5000);
+				result.innerHTML = "YOU LOSE!";
+			}, 4000);
 
 			restart.innerHTML = "Try Again";
 		}
 		else if (computerPoints.innerHTML < userPoints.innerHTML) {
 			var roundsTimerId = setTimeout(function () {
-				result.innerHTML = "Game is over!"
-			}, 3000);
+				result.innerHTML = "GAME OVER!"
+			}, 2000);
 			var resultTimerId = setTimeout(function () {
-				result.innerHTML = "You won!";
-			}, 5000);
+				result.innerHTML = "YOU WON!";
+			}, 4000);
 			restart.innerHTML = "Play Again";
 		}
 		else {
 			var roundsTimerId = setTimeout(function () {
-				result.innerHTML = "Game is over!"
-			}, 3000);
+				result.innerHTML = "GAME OVER!"
+			}, 2000);
 			var resultTimerId = setInterval(function () {
-				result.innerHTML = "It's a tie";
-			}, 5000);
+				result.innerHTML = "IT'S A TIE!";
+			}, 4000);
 			restart.innerHTML = "Play Again";
 		}
 
 		var restartTimerId = setTimeout(function () {
 			restart.style.display = "";
+		}, 7000);
+
+		let choicesDisabled = setTimeout(()=> {
+			document.getElementById('rock').disabled = true;
+			document.getElementById('paper').disabled = true;
+			document.getElementById('scissors').disabled = true;
 		}, 8000);
 	}
 }
@@ -163,19 +170,7 @@ restart.addEventListener("click", function () {
 });
 
 /*button outline property depending on the events*/
-var optionButtons = document.querySelectorAll("div.options button");
-optionButtons.forEach(x => {
-	if (round.innerHTML >= "5") {
-		x.addEventListener("mouseover", () => {
-			x.style.cssText = "outline: none";
-		});
-	}
-	else {
-		x.addEventListener("mouseover", () => {
-			x.style.cssText = "outline: 1px solid #FF0000";
-		});
-	}
-});
+const optionButtons = document.querySelectorAll("div.options button");
 
 optionButtons.forEach(x => {
 	x.addEventListener("mouseout", () => {
